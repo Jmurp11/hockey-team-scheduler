aba5fe1b6f4c96c98ee0b2c15ced0bcd5c3596a4
Jim Murphy <jim.murphy@nera.com>
2025-10-01 17:22:48 -0400

    complete dashboard css

--- app/src/app/dashboard/opponent-list/opponent-card-header/opponent-card-header.component.ts
+++ app/src/app/dashboard/opponent-list/opponent-card-header/opponent-card-header.component.ts
@@ -1,7 +1,7 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { formatLocation } from '../../../shared/utilities/location.utility';
 import { OpponentStatItemComponent } from '../opponent-stat-item/opponent-stat-item.component';
-import {formatLocation} from '../../../shared/utilities/location.utility';
 
 @Component({
   selector: 'app-opponent-card-header',
@@ -21,6 +21,7 @@ import {formatLocation} from '../../../shared/utilities/location.utility';
         </div>
         <div class="stat-info">
           <app-opponent-stat-item
+            class="stat-info__distance"
             label="Distance"
             [value]="formatDistance(opponent.distance)"
           ></app-opponent-stat-item>
@@ -36,7 +37,7 @@ export class OpponentCardHeaderComponent {
   opponent: any;
 
   formatLocation = formatLocation;
-  
+
   formatDistance(distance: string): string {
     return `${distance} mi`;
   }
