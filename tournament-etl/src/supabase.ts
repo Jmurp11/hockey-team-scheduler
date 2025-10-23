d87e8828efe929731dd7a4284f18fddccc4a644d
Jim Murphy <jim.murphy@nera.com>
2025-10-23 18:33:45 -0400

    add tournament etl code

--- tournament-etl/src/supabase.ts
+++ tournament-etl/src/supabase.ts
@@ -1,5 +1,30 @@
-import { createClient } from '@supabase/supabase-js';
-import { env } from 'node:process';
-const supabaseUrl = env.PUBLIC_SUPABASE_URL || '';
-const supabaseKey = env.PUBLIC_SUPABASE_API_KEY || '';
+import { createClient } from "@supabase/supabase-js";
+import { env } from "node:process";
+import { Tournament } from "./types";
+const supabaseUrl = env.PUBLIC_SUPABASE_URL || "";
+const supabaseKey = env.PUBLIC_SUPABASE_API_KEY || "";
 export const supabase = createClient(supabaseUrl, supabaseKey);
+
+export function getTournaments(tournaments: Tournament[]) {
+  try {
+    return supabase
+      .from("tournaments")
+      .select("*")
+      .in(
+        "registrationURL",
+        tournaments.map((t) => t.registrationURL)
+      );
+  } catch (error) {
+    throw new Error("Could not get tournaments: " + (error as Error).message);
+  }
+}
+
+export function insertTournaments(tournaments: Tournament[]) {
+  try {
+    return supabase.from("tournaments").insert(tournaments);
+  } catch (error) {
+    throw new Error(
+      "Could not insert tournaments: " + (error as Error).message
+    );
+  }
+}
--- tournament-etl/src/types.ts
+++ tournament-etl/src/types.ts
@@ -4,3 +4,18 @@ export interface TournamentProps {
   age: string;
   level: string;
 }
+
+export interface Tournament {
+  name: string;
+  city: string;
+  state: string;
+  country: string;
+  rink: string | null;
+  startDate: string;
+  endDate: string;
+  level: string[];
+  age: string[];
+  registrationURL: string;
+  latitude: number;
+  longitude: number;
+}
