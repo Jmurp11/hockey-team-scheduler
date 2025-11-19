da025dc2174aa994d4536d630dd0186313cf3441
Jim Murphy <jim.murphy@nera.com>
2025-11-18 19:48:31 -0500

    start profile page

--- ui/src/app/profile/profile.component.ts
+++ ui/src/app/profile/profile.component.ts
@@ -1,18 +1,40 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { of } from 'rxjs';
+import { ProfileContentComponent } from './profile-content/profile-content.component';
+import { ProfileHeaderComponent } from './profile-header/profile-header.component';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [CommonModule, RouterModule],
-  template: ` 
-    <div>Profile Component</div>`,
-  styleUrl: './profile.component.scss',
+  imports: [
+    CommonModule,
+    RouterModule,
+    ProfileContentComponent,
+    ProfileHeaderComponent,
+  ],
+  template: `@if (profile$ | async; as profile) {
+    <app-profile-header [name]="profile.display_name" />
+    <div class="container">
+      <app-profile-content [card]="profile" />
+    </div>
+    }`,
+  styleUrls: ['./profile.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProfileComponent implements OnInit {
-  
+  profile$ = of({
+    display_name: 'Jim Murphy',
+    association_name: 'Pelham Youth Hockey Association',
+    team_name: ['Pelham Pelicans 16U AA'],
+    user_id: '1beb8d79-b45a-4bed-a1f4-bc738c6ce572',
+    age: ['16u'],
+    association_id: 4918,
+    team_id: 52205,
+    team_rating: 82.77,
+    email: 'murphyj1011@gmail.com',
+  });
 
   ngOnInit(): void {}
 }
---
+++ ui/src/app/shared/types/profile.type.ts
@@ -0,0 +1,9 @@
+export interface Profile {
+  user_id: string;
+  display_name: string;
+  email: string;
+  age: string[];
+  association_name: string;
+  team_name: string[];
+  team_rating: number;
+}
